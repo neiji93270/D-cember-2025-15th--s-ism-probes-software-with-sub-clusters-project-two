94b1413381e2bb09a80c8f26481b747242aaa728
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-12-17 10:29:30 +0100

    Update DecisionOverlapTwoOrSixSidedSibClisterAlgebraForArd.h

--- DecisionOverlapTwoOrSixSidedSibClisterAlgebraForArd.h
+++ DecisionOverlapTwoOrSixSidedSibClisterAlgebraForArd.h
@@ -1,2 +1,4 @@
 //we need 6 ciphers or at least 10 sub clusters
 //to overcome lattices in formalnsoace tonbecome not visible.
+//six ciphers are related to contest larent variables thar may appear in sub algebras
+//on serious high range 3d or 6d olots.
